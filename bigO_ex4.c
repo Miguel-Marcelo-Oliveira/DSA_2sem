d105f5f9131e219c624768e855c70f58774703c9
Miguel-Marcelo-Oliveira <miguel.marceloramos@gmail.com>
2026-08-18 22:01:38 -0300

    Adicionada a resolução do exercício 4

--- bigO_ex4.c
+++ bigO_ex4.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 
-void mostrarDobros(int n)
-{
-
-    for (int i = 1; i < n; i *= 2)
-    {
-        printf("%d\n", i);
+void mostrarDobros(int n) {
+    for (int i = 1; i < n; i *= 2) {
+        printf("%d\n", i);  // O(1)
     }
 }
 
-int main(void)
-{
+// O valor de i dobra a cada loop, até que a condição se torne falsa
+// toda vez que n é dobrado, o algoritmo precisa de apenas mais um passo
 
+// 2**k = n
+// k = log2(n)
+// 2 elevado a k é igual a n
+// sendo k o número de vezes que o loop é executado
+
+int main(void){
     mostrarDobros(32);
-
-    return 0;
+    return 0; // O(1)
 }
+
+
+// Big O final ==> O(log n)
+// g(n) final ==> g(n) = log2(n)
